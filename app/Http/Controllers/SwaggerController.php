ccdf96b9274db87baa1e9caf0f9e080cf5721963
管宜尧 <guanyiyao@yunsom.com>
2019-01-02 15:49:21 +0800

    生成文档

---
+++ app/Http/Controllers/SwaggerController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * swagger-demo
+ *
+ * @link      https://www.yunsom.com/
+ * @copyright 管宜尧 <guanyiyao@yunsom.com>
+ */
+
+namespace App\Http\Controllers;
+
+
+class SwaggerController
+{
+
+}
